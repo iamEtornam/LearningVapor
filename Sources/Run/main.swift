4552c30a3515f6f266febb205f86e09e493e19f1
Etornam Sunu Bright <sunumacbright@gmail.com>
2019-11-03 10:36:20 +0000

    added comments

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -3,12 +3,19 @@ import Vapor
 let app = try Application()
 let router = try app.make(Router.self)
 
+//perform a get method
 router.get("hello") { request in
     return "Hello, world."
 }
 
-router.post("names"){ request in
-    return "hi"
-}
+//perform a post method. this takes a string and displays it in the response
+//router.get("names") { req in
+//    guard let name = request.data["name"]?.string else{
+//        throw Abort.badRequest
+//    }
+//    return try JSON(
+//        node:[
+//        "message":"Hello, \(name)!"])
+//}
 
 try app.run()
